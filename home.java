1434bacc25e1b4a46e6c81884b42bbc9e0263f4e
Arvind <ak5943436@gmail.com>
2025-07-30 05:57:42 +0000

    dfas

--- home.java
+++ home.java
@@ -1,1 +1,1 @@
-public class home{public static void main(String []args){System.out.println("hello kinsssssssss");}}
+public class home{public static void main(String []args){System.out.println("helloooo kinsssssssss");}}
